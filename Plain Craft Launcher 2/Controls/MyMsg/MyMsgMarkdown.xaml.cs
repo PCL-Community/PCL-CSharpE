50e96136731a3da45efdc43d324757c5a26ccd16
薄奚梦灵 <boximengling@outlook.com>
2026-02-22 15:58:24 +0800

    fix(msgbox): add click event

--- Plain Craft Launcher 2/Controls/MyMsg/MyMsgMarkdown.xaml.cs
+++ Plain Craft Launcher 2/Controls/MyMsg/MyMsgMarkdown.xaml.cs
@@ -108,7 +108,7 @@ public partial class MyMsgMarkdown
         }, "MyMsgBox " + Uuid);
     }
 
-    public void Btn1_Click(o)
+    public void Btn1_Click(object sender,EventArgs e)
     {
         if (MyConverter.IsExited)
             return;
@@ -124,7 +124,7 @@ public partial class MyMsgMarkdown
         }
     }
 
-    public void Btn2_Click()
+    public void Btn2_Click(object sender,EventArgs e)
     {
         if (MyConverter.IsExited)
             return;
@@ -140,7 +140,7 @@ public partial class MyMsgMarkdown
         }
     }
 
-    public void Btn3_Click()
+    public void Btn3_Click(object sender,EventArgs e)
     {
         if (MyConverter.IsExited)
             return;
